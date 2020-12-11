99b3002151a990daa2898ff6817abf70efb94877
Ryan Poe <baublet@gmail.com>
2020-12-10 22:43:44 -0600

    request instead dummy

--- server.js
+++ server.js
@@ -1,55 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const puppeteer = require('puppeteer-extra')
+const request = require("request");
 
-// add stealth plugin and use defaults (all evasion techniques)
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
-
-async function getAddToCartButton(page) {
-  try {
-    const addToCart = await page.waitForSelector(
-      `button[data-product-code="3005816"].add-to-cart`,
-      {
-        timeout: 3000,
-      }
-    );
-    const className = await (
-      await addToCart.getProperty("className")
-    ).jsonValue();
-    return {
-      className,
-      error: {},
-    };
-  } catch (e) {
-    return {
-      className: "NOT FOUND!",
-      error: e,
-    };
-  }
-}
-
-async function getPage() {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--window-size=1920,1080"],
-  });
-  const page = await browser.newPage();
-
-
-  await page.goto(
-    "https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816"
-  );
-
-  const { className, error } = await getAddToCartButton(page);
-
-  const pageHtml = await page.evaluate(() => document.body.innerHTML);
-  await browser.close();
-  return {
-    addToCartButtonClassName: className,
-    pageHtml,
-    error,
+function getStatus() {
+  var headers = {
+    authority: "api.direct.playstation.com",
+    pragma: "no-cache",
+    "cache-control": "no-cache",
+    accept: "*/*",
+    "user-agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "content-type": "application/json",
+    origin: "https://direct.playstation.com",
+    "sec-fetch-site": "same-site",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    referer: "https://direct.playstation.com/",
+    "accept-language": "en-US,en;q=0.9",
   };
+  var options = {
+    url:
+      "https://api.direct.playstation.com/commercewebservices/ps-direct-us/users/anonymous/products/productList?fields=BASIC&productCodes=3005816",
+    headers: headers,
+  };
+  return new Promise((resolve) => {
+    request(options, (error, response, body) => {
+      let available = false;
+      if (!error && response.statusCode == 200) {
+        available = !body.includes(`"stockLevelStatus":"outOfStock"`);
+      }
+
+      resolve({
+        available,
+        body,
+        error,
+      });
+    });
+  });
 }
 
 function escapeHTML(s = "") {
@@ -71,31 +59,30 @@ app.get("/", async (req, res) => {
   <div class="hide">Loading...</div>
   `
   );
-  const { addToCartButtonClassName, pageHtml, error } = await getPage();
+  const { available, error, body } = await getStatus();
   res.write(`
   <style type="text/css">.hide { display: none }</style>
 
   ${
-    `${addToCartButtonClassName}`.includes("hide")
+    error
+      ? "<h2 style='color:yellow'>Unknown (error)</h2>"
+      : !available
       ? "<h2 style='color:red'>NO</h2>"
       : "<h2 style='color:green' yes-it-is-available-foolio>YES</h2>"
   }
-
-  <br><br>
   <hr />
 
-  <b>Search method:</b> addToCart button classname
-  <br><br></b> Value: [${addToCartButtonClassName}]
+  <b>Search method:</b> JSON API
 
-  <br><br><br><br><hr />
-  <h1>Page HTML</h1>
-  <textarea style="width:100%;height:500px">${escapeHTML(pageHtml)}</textarea>
+<hr />
+  <h1>JSON Result</h1>
+  <textarea style="width:100%;height:500px">${escapeHTML(body)}</textarea>
 
-  <br><br><br><br><hr />
+<hr />
   <h1>Error</h1>
   <textarea style="width:100%;height:500px">${escapeHTML(
-    error.message
-  )}\n\n${escapeHTML(error.stack)}</textarea>
+    error || ""
+  )}</textarea>
 
   </body>
   </html>
