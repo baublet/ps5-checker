d22bdbe2037339b806990f86296afc57bcf4b6ce
Ryan Poe <baublet@gmail.com>
2020-12-10 22:18:44 -0600

    partial response then final

--- server.js
+++ server.js
@@ -3,29 +3,50 @@ const app = express();
 const port = 3000;
 const puppeteer = require("puppeteer");
 
+async function getAddToCartButton(page) {
+  try {
+    const addToCart = await page.waitForSelector(
+      `button[data-product-code="3005816"].add-to-cart`,
+      {
+        timeout: 3000,
+      }
+    );
+    const className = await (
+      await addToCart.getProperty("className")
+    ).jsonValue();
+    return {
+      className,
+      error: {},
+    };
+  } catch (e) {
+    return {
+      className: "NOT FOUND!",
+      error: e,
+    };
+  }
+}
+
 async function getPage() {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: ["--no-sandbox"],
   });
   const page = await browser.newPage();
   await page.goto(
     "https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816"
   );
-  const addToCart = await page.waitForSelector(
-    `button[data-product-code="3005816"].add-to-cart`
-  );
-  const className = await (
-    await addToCart.getProperty("className")
-  ).jsonValue();
+
+  const { className, error } = await getAddToCartButton(page);
+
   const pageHtml = await page.evaluate(() => document.body.innerHTML);
   await browser.close();
   return {
     addToCartButtonClassName: className,
     pageHtml,
+    error,
   };
 }
 
-function escapeHTML(s) {
+function escapeHTML(s = "") {
   return s
     .replace(/&/g, "&amp;")
     .replace(/"/g, "&quot;")
@@ -34,13 +55,20 @@ function escapeHTML(s) {
 }
 
 app.get("/", async (req, res) => {
-  const { addToCartButtonClassName, pageHtml } = await getPage();
-  res.send(`
+  res.write(
+    `
   <html>
   <head><title>Is the PS5 Available?</title></head>
   <body>
 
   <h1>Is the PS5 Available?</h1>
+  <div class="hide">Loading...</div>
+  `
+  );
+  const { addToCartButtonClassName, pageHtml, error } = await getPage();
+  res.write(`
+  <style type="text/css">.hide { display: none }</style>
+
   ${
     `${addToCartButtonClassName}`.includes("hide")
       ? "<h2 style='color:red'>NO</h2>"
@@ -57,10 +85,17 @@ app.get("/", async (req, res) => {
   <h1>Page HTML</h1>
   <textarea style="width:100%;height:500px">${escapeHTML(pageHtml)}</textarea>
 
+  <br><br><br><br><hr />
+  <h1>Error</h1>
+  <textarea style="width:100%;height:500px">${escapeHTML(
+    error.message
+  )}\n\n${escapeHTML(error.stack)}</textarea>
+
   </body>
   </html>
   
   `);
+  res.end();
 });
 
 app.listen(port, () => {
